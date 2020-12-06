a98072389480b4b649c53da8e33a666004b3dad5
claudinemahoro <claudinemah3@gmail.com>
2020-12-06 14:03:18 +0200

    making start.sh to run our app

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@ export SECRET_KEY=002
 export DATABASE_URL=postgresql+psycopg2://claudine:1998@localhost/blogs
 export MAIL_USERNAME=claudinemah3@gmail.com
 export MAIL_PASSWORD=winner@98
+python3.6 manage.py run
